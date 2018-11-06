22964b413fe67cef0fe0b97d391aef6b1aa6d6cd
btow <btow@yandex.ru>
2018-11-06 23:42:24 +0500

    Initial commit

---
+++ src/Buble.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by btow on 06.11.2018.
+ */
+class Buble(var charArray: CharArray) {
+
+    fun getSortedArray() : CharArray {
+        val end = charArray.size - 1
+        for (i in end downTo 1) {
+            for (j in 1..i)
+                if (charArray[j - 1] > charArray[j]){
+                    val temp = charArray[j]
+                    charArray[j] = charArray[j - 1]
+                    charArray[j - 1] = temp
+                }
+        }
+        return charArray
+    }
+}
---
+++ src/BubleTest.kt
@@ -0,0 +1,26 @@
+import org.junit.Assert.*
+import java.util.*
+
+/**
+ * Created by btow on 06.11.2018.
+ */
+class BubleTest : Exception() {
+
+    lateinit var noSortAfrray: CharArray
+    lateinit var expSortArray: CharArray
+
+    @org.junit.Before
+    fun setUp() {
+        noSortAfrray = charArrayOf('S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E')
+        expSortArray = noSortAfrray.copyOf()
+        Arrays.sort(expSortArray)
+    }
+
+    @org.junit.Test
+    fun getSortedArray() {
+
+        val actSortedArray = Buble(noSortAfrray).getSortedArray()
+
+        assertArrayEquals(expSortArray, actSortedArray)
+    }
+}
