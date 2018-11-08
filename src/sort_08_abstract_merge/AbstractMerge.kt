a593896a33964e0cb749cf9d35fe102387a8e27b
btow <btow@yandex.ru>
2018-11-09 00:30:47 +0500

    Next sixth commit

--- src/sort_08_abstract_merge/AbstractMerge.kt
+++ src/sort_08_abstract_merge/AbstractMerge.kt
@@ -4,21 +4,39 @@ package sort_08_abstract_merge
  * Created by btow on 07.11.2018.
  * Сортировка выыбором (Шелла) до O(log n)
  */
-class AbstractMerge(var charArray: CharArray) {
+class AbstractMerge(val charArray: CharArray) {
 
     fun getSortedArray(): CharArray {
-        val newCharArray = charArray.copyOf()
         val end = charArray.size - 1
-        var left = 0
-        var mid = end/2
-        var right = mid + 1
-        for (k in left..end){
-            if (left > mid) charArray[k] = newCharArray[right++]
-            else if (right > end) charArray[k] = newCharArray[left++]
-            else if (newCharArray[right] < newCharArray[left]) charArray[k] = newCharArray[right++]
-            else charArray[k] = newCharArray[left++]
-        }
+        val left = 0
+        val mid = end / 2
+        return sortArray(charArray, left, mid, end)
+    }
 
-        return charArray
+    private fun sortArray(charArray: CharArray, left: Int, mid: Int, end: Int): CharArray {
+        val newCharArray = CharArray(charArray.size)
+        if (charArray.size > 2) {
+            val leftCharArray = sortArray(
+                    charArray.copyOfRange(left, mid + 1), 0, mid / 2, mid)
+            val rigthCharArray = sortArray(
+                    charArray.copyOfRange(mid + 1, end + 1), 0, (end - mid + 2) / 2, (end - mid + 2))
+            var lft = left
+            var right = 0
+
+            for (k in lft..end) {
+                if (lft > mid)
+                    newCharArray[k] = rigthCharArray[right++]
+                else if (right >= rigthCharArray.size)
+                    newCharArray[k] = leftCharArray[lft++]
+                else if (rigthCharArray[right] < leftCharArray[lft])
+                    newCharArray[k] = rigthCharArray[right++]
+                else
+                    newCharArray[k] = leftCharArray[lft++]
+            }
+        } else if (charArray[0] > charArray[1]) {
+            newCharArray[0] = charArray[1]
+            newCharArray[1] = charArray[0]
+        }
+        return newCharArray
     }
 }
